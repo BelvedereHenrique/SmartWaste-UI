0b35622ac985de2bc30f86c20cdf6caf4156a639
Felipe Botelho Rodrigues <felipebotelhofelipe@hotmail.com>
2017-03-03 13:27:33 -0300

    fix bugs on enterprise add, insert directive loading

--- src/app/account-menu/account-menu.component.ts
+++ src/app/account-menu/account-menu.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core"
 import { Router } from "@angular/router"
 
+import { NotificationService, Notification, NotificationResult, NotificationButton } from '../_shared/_services/notification.service'
+import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
 import { AccountEnterpriseService } from "../_shared/_services/account-enterprise.service";
 //import { NotificationComponent, Notification } from "./notification.component"
 
@@ -10,13 +12,17 @@ import { AccountEnterpriseService } from "../_shared/_services/account-enterpris
     styleUrls: ["./account-menu.component.css"]
 })
 
+
 export class AccountMenuComponent {
-    constructor(private router: Router, private _accountService: AccountEnterpriseService) {
-        this.mockUser();
+    constructor(private router: Router, private _accountService: AccountEnterpriseService, private _notificationService: NotificationService, private slimLoadingBarService: SlimLoadingBarService ) {
+        this.CheckUserEnterprise();
         console.log(this.retrieve());
         //check can make a enterprise
 
     }
+    public canShowEnterpriseMenu = false;
+    public enterprise = null;
+    private isLoading = false;
     public onRequestEnterprisePermissionClick(): void {
         this.router.navigateByUrl("account/enterprise");
         //this.n.Notify(new Notification("Teste", [], 15000));
@@ -30,19 +36,25 @@ export class AccountMenuComponent {
         localStorage.setItem("currentUser", JSON.stringify({ user: this.currentUser }));
     }
     private CheckUserEnterprise():void{
-      /*this._accountService.CheckUserEnterprise(this.AccountEnterprise).subscribe(
-        data => {
+      this.slimLoadingBarService.start();
+      this._accountService.getUserEnterprise().subscribe(
+      data => {
             if(data.Success == true){
-            
-            }else{
-            
+                debugger
+                if(data.Result.ID != null){
+                    this.enterprise = data.Result;
+                    this.canShowEnterpriseMenu = false;
+                }else{
+                    this.canShowEnterpriseMenu = true;
+                }
             }
-          },
-        error => {
-         
         },
-        () => this.isLoading = false
-      );*/
+        error => {
+          console.log(error)
+          this._notificationService.notify(new Notification("An Error Ocurred on Server",[],3000));
+        },
+        () =>  this.slimLoadingBarService.complete()
+        );
     }
 
     private retrieve() {
